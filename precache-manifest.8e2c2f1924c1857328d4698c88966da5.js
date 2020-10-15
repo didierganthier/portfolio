f9b55df9cddd60fb62ebf948704dd83a723ac353
didierganthier <didierganthierperan@gmail.com>
2020-10-15 13:06:32 -0400

    Updates

---
+++ precache-manifest.8e2c2f1924c1857328d4698c88966da5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d59f08fd7619f3a6654fc08e32384d91",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cfd6d6a75a3a46d8b3b5",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bda8fd9d8614e957af3e",
+    "url": "/portfolio/static/js/2.347b21a0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/portfolio/static/js/2.347b21a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfd6d6a75a3a46d8b3b5",
+    "url": "/portfolio/static/js/main.e65a6a8c.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
