9a26dcf61f4e61f71a18b15af950bf5f60dfb57e
didierganthier <didierganthierperan@gmail.com>
2020-10-15 13:37:28 -0400

    fluid property modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
 import './App.css';
 
 class App extends React.Component {
@@ -29,7 +30,11 @@ class App extends React.Component {
 
   render (){
     return(
-      <div>Hello from React</div>
+      <Router>
+        <Container fluid={false}>
+          <p>Hi from React</p>
+        </Container>
+      </Router>
     );
   }
 }
