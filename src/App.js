0af60321ff86be87ceb41fa4599bc26f7438625b
didierganthier <didierganthierperan@gmail.com>
2020-10-15 13:00:27 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Didier Peran Ganthier',
+      headerLinks: [
+        { title: 'Home', path: '/' },
+        { title: 'About', path: '/about' },
+        { title: 'Contact', path: '/contact' },
+      ],
+      home: {
+        title: 'Be Limitless',
+        subtitle: 'Creative thinking never ends',
+        text: 'Checkout my projects below'
+      },
+      about: {
+        title: 'About Me',
+      },
+      contact: {
+        title: 'Let\'s talk',
+      },
+    }   
+  }
+
+  render (){
+    return(
+      <div>Hello from React</div>
+    );
+  }
+}
+
+export default App;
