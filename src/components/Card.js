a63bbb1079171234268da2c1e65f14f31c612bfb
didierganthier <didierganthierperan@gmail.com>
2020-10-16 10:41:28 -0400

    CardInfo designed and called in Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import CardInfo from '../components/CardInfo'
 
 function Card(props){
     return(
-        <p>Card Works!</p>
+        <div>
+            <img className="" src={props.item.imgSrc} alt="project"/>
+            { props.item.selected && <CardInfo title={props.item.title} subTitle={props.item.subTitle} link={props.item.link}/> }
+        </div>
     );
 }
 
---
+++ src/components/CardInfo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function CardInfo(props){
+    return(
+        <p>Card Info</p>
+    );
+}
+
+export default CardInfo;
