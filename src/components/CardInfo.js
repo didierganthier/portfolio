32fa2ae0286ccc17e05076434cb3d68fe1c44e1c
didierganthier <didierganthierperan@gmail.com>
2020-10-16 10:53:35 -0400

    animated div added in CardInfo

--- src/components/CardInfo.js
+++ src/components/CardInfo.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { useSpring, animated } from 'react-spring';
 
 function CardInfo(props){
+
+    const style = useSpring({opacity: 1, from:{opacity: 0}});
+
     return(
-        <p>Card Info</p>
+        <animated.div className="" style={style}>
+            <p>{props.title}</p>
+            <p>{props.subTitle}</p>
+            <a href={props.link} target="_blank" rel="noopener noreferrer">View</a>
+        </animated.div>
     );
 }
 
